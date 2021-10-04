368cb6d959c5df9ce36156da31257bbeb4ff24c4
wormjoo <wormjoo@naver.com>
2021-10-05 05:40:25 +0900

    feat(home): 네비게이션 생성

---
+++ capstone-expo/src/navigations/bottom-tab.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/home';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const TabIcon = ({ name, size, color }) => {
+  return <MaterialCommunityIcons name={name} size={size} color={color} />;
+};
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="홈"
+        component={Home}
+        options={{
+          tabBarIcon: (props) => TabIcon({ ...props, name: 'home' }),
+        }}
+      />
+      <Tab.Screen
+        name="도서관"
+        component={Home}
+        options={{
+          tabBarIcon: (props) => TabIcon({ ...props, name: 'library' }),
+        }}
+      />
+      <Tab.Screen
+        name="책가방"
+        component={Home}
+        options={{
+          tabBarIcon: (props) => TabIcon({ ...props, name: 'bag-personal' }),
+        }}
+      />
+      <Tab.Screen
+        name="마이페이지"
+        component={Home}
+        options={{
+          tabBarIcon: (props) => TabIcon({ ...props, name: 'account' }),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigation;
