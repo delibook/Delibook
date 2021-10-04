e44d0ed8c3f5f3d846a61dbba50ed093ade02385
wormjoo <wormjoo@naver.com>
2021-10-05 04:36:22 +0900

    feat(home): 네비게이션 생성

---
+++ capstone-expo/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./theme";
+import TabNavigation from "./navigations/bottom-tab";
+import { NavigationContainer } from "@react-navigation/native";
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer>
+        <TabNavigation />
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ capstone-expo/src/screens/home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import { Button } from "react-native";
+
+const Container = styled.View`
+  align-items: center;
+`;
+
+const StyledText = styled.Text`
+  font-size: 30px;
+  margin-bottom: 10px;
+`;
+
+const Home = () => {
+  return (
+    <Container>
+      <StyledText>Home</StyledText>
+    </Container>
+  );
+};
+
+export default Home;
