1423ea4efe196820637d24b9c8322550a287c800
juy4556 <70626308+juy4556@users.noreply.github.com>
2021-10-08 01:05:12 +0900

    style(join): change style

--- capstone-expo/src/screens/join.js
+++ capstone-expo/src/screens/join.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  Button,
   View,
   ScrollView,
   Text,
@@ -10,6 +9,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import { EvilIcons } from '@expo/vector-icons';
+import { Button } from '../components';
 
 const Join = ({ navigation }) => {
   const [email, setEmail] = React.useState('');
@@ -22,7 +22,16 @@ const Join = ({ navigation }) => {
   const [checkbox3, setCheckbox3] = React.useState(false);
   const [checkbox4, setCheckbox4] = React.useState(false);
   const [checkbox5, setCheckbox5] = React.useState(false);
+  const [disabled, setDisabled] = React.useState(true);
 
+  const _handleJoinButtonPress = async () => {
+    try {
+      spinner.start();
+    } catch (e) {
+    } finally {
+      spinner.stop();
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -33,7 +42,7 @@ const Join = ({ navigation }) => {
             width: 20,
             height: 20,
             left: 30,
-            top: 10,
+            top: 21,
             zIndex: 10,
           }}
           name="close"
@@ -42,7 +51,6 @@ const Join = ({ navigation }) => {
         <Text
           style={{
             textAlign: 'center',
-            marginTop: -15,
             fontSize: 17,
             fontWeight: '500',
           }}
@@ -50,8 +58,10 @@ const Join = ({ navigation }) => {
           회원가입
         </Text>
       </View>
-      <ScrollView style={{ width: '100%', alignSelf: 'center', padding: 30 }}>
-        <Text>이메일 주소</Text>
+      <ScrollView
+        style={{ flex: 1, width: '100%', alignSelf: 'center', padding: 30 }}
+      >
+        <Text style={styles.label}>이메일 주소</Text>
         <TextInput
           style={styles.input}
           value={email}
@@ -59,7 +69,7 @@ const Join = ({ navigation }) => {
           keyboardType={'email-address'}
           placeholder="이메일 주소 입력"
         />
-        <Text>비밀번호</Text>
+        <Text style={styles.label}>비밀번호</Text>
         <TextInput
           style={styles.input}
           secureTextEntry={true}
@@ -67,14 +77,14 @@ const Join = ({ navigation }) => {
           onChangeText={setPassword}
           placeholder="영문, 숫자, 특수문자 포함 8자리 이상"
         />
-        <Text>이름</Text>
+        <Text style={styles.label}>이름</Text>
         <TextInput
           style={styles.input}
           value={name}
           onChangeText={setName}
           placeholder="이름 입력"
         />
-        <Text>휴대폰 번호</Text>
+        <Text style={styles.label}>휴대폰 번호</Text>
         <TextInput
           style={styles.input}
           value={phone}
@@ -84,12 +94,12 @@ const Join = ({ navigation }) => {
         />
         <TouchableOpacity
           onPress={() => {
-            setAllCheckbox(!allCheckbox);
-            setCheckbox1(!checkbox1);
-            setCheckbox2(!checkbox2);
-            setCheckbox3(!checkbox3);
-            setCheckbox4(!checkbox4);
-            setCheckbox5(!checkbox5);
+            setAllCheckbox(true);
+            setCheckbox1(true);
+            setCheckbox2(true);
+            setCheckbox3(true);
+            setCheckbox4(true);
+            setCheckbox5(true);
           }}
           style={{ flexDirection: 'row', paddingBottom: 15 }}
         >
@@ -110,7 +120,7 @@ const Join = ({ navigation }) => {
             onPress={() => {
               setCheckbox1(!checkbox1);
             }}
-            style={{ flexDirection: 'row' }}
+            style={styles.touchable}
           >
             <Image
               style={styles.Checkbox}
@@ -120,9 +130,7 @@ const Join = ({ navigation }) => {
                   : require('../../assets/blank-check-box.png')
               }
             ></Image>
-            <Text style={{ paddingLeft: 5, marginTop: 2, fontSize: 20 }}>
-              (필수)만 14세 이상입니다.
-            </Text>
+            <Text style={styles.checkText}>(필수)만 14세 이상입니다.</Text>
             <Image
               style={styles.list}
               source={require('../../assets/list.png')}
@@ -132,7 +140,7 @@ const Join = ({ navigation }) => {
             onPress={() => {
               setCheckbox2(!checkbox2);
             }}
-            style={{ flexDirection: 'row' }}
+            style={styles.touchable}
           >
             <Image
               style={styles.Checkbox}
@@ -142,7 +150,7 @@ const Join = ({ navigation }) => {
                   : require('../../assets/blank-check-box.png')
               }
             ></Image>
-            <Text style={{ paddingLeft: 5, marginTop: 2, fontSize: 20 }}>
+            <Text style={styles.checkText}>
               (필수)개인정보 수집 및 이용동의
             </Text>
             <Image
@@ -154,7 +162,7 @@ const Join = ({ navigation }) => {
             onPress={() => {
               setCheckbox3(!checkbox3);
             }}
-            style={{ flexDirection: 'row' }}
+            style={styles.touchable}
           >
             <Image
               style={styles.Checkbox}
@@ -164,9 +172,7 @@ const Join = ({ navigation }) => {
                   : require('../../assets/blank-check-box.png')
               }
             ></Image>
-            <Text style={{ paddingLeft: 5, marginTop: 2, fontSize: 20 }}>
-              (필수)딜리북 이용약관 동의
-            </Text>
+            <Text style={styles.checkText}>(필수)딜리북 이용약관 동의</Text>
             <Image
               style={styles.list}
               source={require('../../assets/list.png')}
@@ -176,7 +182,7 @@ const Join = ({ navigation }) => {
             onPress={() => {
               setCheckbox4(!checkbox4);
             }}
-            style={{ flexDirection: 'row' }}
+            style={styles.touchable}
           >
             <Image
               style={styles.Checkbox}
@@ -186,7 +192,7 @@ const Join = ({ navigation }) => {
                   : require('../../assets/blank-check-box.png')
               }
             ></Image>
-            <Text style={{ paddingLeft: 5, marginTop: 2, fontSize: 20 }}>
+            <Text style={styles.checkText}>
               (필수)전자금융거래 이용약관 동의
             </Text>
             <Image
@@ -198,7 +204,7 @@ const Join = ({ navigation }) => {
             onPress={() => {
               setCheckbox5(!checkbox5);
             }}
-            style={{ flexDirection: 'row' }}
+            style={styles.touchable}
           >
             <Image
               style={styles.Checkbox}
@@ -208,21 +214,18 @@ const Join = ({ navigation }) => {
                   : require('../../assets/blank-check-box.png')
               }
             ></Image>
-            <Text
-              style={{
-                paddingLeft: 5,
-                marginTop: 2,
-                fontSize: 20,
-              }}
-            >
-              (필수)개인정보 제공 동의
-            </Text>
+            <Text style={styles.checkText}>(필수)개인정보 제공 동의</Text>
             <Image
               style={styles.list}
               source={require('../../assets/list.png')}
             />
           </TouchableOpacity>
         </View>
+        <Button
+          title="가입하기"
+          onPress={_handleJoinButtonPress}
+          disabled={disabled}
+        />
       </ScrollView>
     </View>
   );
@@ -238,12 +241,12 @@ const styles = StyleSheet.create({
   input: {
     marginTop: 10,
     marginBottom: 20,
+    paddingLeft: 10,
     height: 40,
     borderWidth: 1,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
+    paddingVertical: 1,
     borderColor: '#b6b6b6',
-    borderRadius: 3,
+    borderRadius: 4,
   },
   header: {
     height: 60,
@@ -253,22 +256,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   Checkbox: {
-    width: 25,
-    height: 25,
+    width: 20,
+    height: 20,
     zIndex: 10,
   },
   Checkboxes: {
-    flex: 0.5,
+    flex: 1,
     height: 180,
-    paddingTop: 20,
+    paddingTop: 15,
     paddingLeft: 20,
     borderRadius: 10,
     borderColor: 'black',
     borderWidth: 0.25,
   },
   list: {
-    width: 25,
-    height: 25,
+    width: 20,
+    height: 20,
+  },
+  checkText: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    marginTop: 2,
+    fontSize: 18,
+  },
+  touchable: {
+    flexDirection: 'row',
+    paddingBottom: 7,
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: '600',
+    marginBottom: 6,
   },
 });
 
