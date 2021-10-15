bb4e99475c5cb67a730258bf9f443d0397314dc3
juy4556 <70626308+juy4556@users.noreply.github.com>
2021-10-15 16:36:00 +0900

    feat(library):add libraryList

--- capstone-expo/src/screens/library.js
+++ capstone-expo/src/screens/library.js
@@ -10,150 +10,275 @@ import {
   TextInput,
   TouchableOpacity,
   Button,
-  Dimensions,
-  StatusBar,
-  Alert,
-  Animated as RNAnimated,
-  Easing as RNAnimatedEasing,
+  Pressable,
 } from 'react-native';
+import axios from 'axios';
 import { Ionicons } from '@expo/vector-icons';
-import ReAnimated, {
-  Easing as ReAnimatedEasing,
-} from 'react-native-reanimated';
-import Constants from 'expo-constants';
-
-const { width, height } = Dimensions.get('window');
-
-const searchBarHeight = 50;
-const topPosition = Constants.statusBarHeight;
-const centerPosition = (height - searchBarHeight) / 2;
-
-const useReanmiated = false;
-const Animated = useReanmiated ? ReAnimated : RNAnimated;
-const Easing = useReanmiated ? ReAnimatedEasing : RNAnimatedEasing;
 
 const Library = ({ navigation }) => {
-  const [name, setName] = useState('');
-  const [isOpened, setIsOpened] = React.useState(false);
-  const animPosition = React.useRef(new Animated.Value(centerPosition));
-  const animWidth = React.useRef();
-  const animOpacity = React.useRef();
-  const textRef = React.useRef();
-
-  const onFocus = () => {
-    setIsOpened(true);
-    Animated.timing(animPosition.current, {
-      toValue: topPosition,
-      duration: 300,
-      easing: Easing.out(Easing.ease),
-    }).start();
-  };
-
-  const onBlur = () => {
-    Animated.timing(animPosition.current, {
-      toValue: centerPosition,
-      duration: 300,
-      easing: Easing.in(Easing.ease),
-    }).start(() => setIsOpened(false));
-  };
-
-  animWidth.current = animPosition.current.interpolate({
-    inputRange: [topPosition, centerPosition],
-    outputRange: [width, width * 0.8],
-  });
-
-  animOpacity.current = animPosition.current.interpolate({
-    inputRange: [0, centerPosition],
-    outputRange: [1, 0],
-  });
+  const [libName, setLibName] = useState('');
+  const [name, setName] = useState('판교어린이도서관');
+  const [addr, setAddr] = useState('경기도 성남시');
+  const [rest, setRest] = useState('공휴일');
+  const [price, setPrice] = useState('1000원');
 
   return (
     <View style={styles.container}>
-      <Animated.View
-        style={{
-          opacity: animOpacity.current,
-          backgroundColor: 'green',
-          paddingTop: searchBarHeight,
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        {isOpened && (
-          <ScrollView keyboardShouldPersistTaps={'always'}>
-            {new Array(250).fill(Math.random()).map((val, index) => {
-              return (
-                <Text
-                  style={{
-                    padding: 10,
-                    backgroundColor: 'white',
-                    marginBottom: 2,
-                  }}
-                  key={val}
-                  onPress={() => alert('press:' + index)}
-                >
-                  검색내역: {index}
-                </Text>
-              );
-            })}
-          </ScrollView>
-        )}
-      </Animated.View>
-
-      <Animated.View
-        style={{
-          borderWidth: 1,
-          position: 'absolute',
-          alignSelf: 'center',
-          justifyContent: 'center',
-          height: searchBarHeight,
-          width: animWidth.current,
-          top: animPosition.current,
-        }}
-      >
-        <TextInput
-          ref={textRef}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          style={{
-            backgroundColor: '#c8c8c8',
-            width: '100%',
-            height: '100%',
-            paddingHorizontal: 10,
-            fontSize: 14,
-          }}
-        />
-        {isOpened && (
-          <Animated.Text
-            onPress={textRef.current.blur}
+      <View style={styles.searchButton}>
+        <Pressable
+          style={styles.input}
+          value={libName}
+          onChangeText={setLibName}
+        >
+          <TextInput
             style={{
-              position: 'absolute',
+              left: 20,
+              fontSize: 15,
+              alignItems: 'center',
+              justifyContent: 'center',
+              color: '#d5d5d5',
+            }}
+            placeholder="도서관명을 입력해주세요."
+          ></TextInput>
+          <Ionicons
+            onPress={() => console.log(`search`)}
+            style={{
+              zIndex: 10,
               right: 10,
-              padding: 10,
-              opacity: animOpacity.current,
+              top: 8,
+            }}
+            name="search-outline"
+            size={25}
+            color="#75CBF3"
+          />
+        </Pressable>
+      </View>
+      <ScrollView style={{ flex: 1 }}>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => navigation.navigate('Mypage', { name: 'Mypage' })}
+        >
+          <Text style={{ fontSize: 20, paddingTop: 10, paddingBottom: 10 }}>
+            {name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
             }}
           >
-            {'닫기'}
-          </Animated.Text>
-        )}
-      </Animated.View>
-      <View style={styles.navigation}>
-        <TextInput
-          style={styles.input}
-          value={name}
-          onChangeText={setName}
-          placeholder="도서관명을 입력해주세요."
-        ></TextInput>
-        <Ionicons
-          onPress={() => console.log(`search`)}
-          style={{
-            textAlign: 'center',
-            zIndex: 10,
-          }}
-          name="search-outline"
-          size={35}
-          color="#75CBF3"
-        />
-      </View>
+            <Text>{addr}</Text>
+            <Text>공공도서관</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>휴관일 | {rest}</Text>
+            <Text style={{ color: '#3679fe' }}>{price}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => navigation.navigate('Mypage')}
+        >
+          <Text style={{ fontSize: 20, paddingTop: 10, paddingBottom: 10 }}>
+            {name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>{addr}</Text>
+            <Text>공공도서관</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>휴관일 | {rest}</Text>
+            <Text style={{ color: '#3679fe' }}>{price}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => navigation.navigate('Mypage')}
+        >
+          <Text style={{ fontSize: 20, paddingTop: 10, paddingBottom: 10 }}>
+            {name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>{addr}</Text>
+            <Text>공공도서관</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>휴관일 | {rest}</Text>
+            <Text style={{ color: '#3679fe' }}>{price}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => navigation.navigate('Mypage')}
+        >
+          <Text style={{ fontSize: 20, paddingTop: 10, paddingBottom: 10 }}>
+            {name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>{addr}</Text>
+            <Text>공공도서관</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>휴관일 | {rest}</Text>
+            <Text style={{ color: '#3679fe' }}>{price}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => navigation.navigate('Mypage')}
+        >
+          <Text style={{ fontSize: 20, paddingTop: 10, paddingBottom: 10 }}>
+            {name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>{addr}</Text>
+            <Text>공공도서관</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>휴관일 | {rest}</Text>
+            <Text style={{ color: '#3679fe' }}>{price}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => navigation.navigate('Mypage')}
+        >
+          <Text style={{ fontSize: 20, paddingTop: 10, paddingBottom: 10 }}>
+            {name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>{addr}</Text>
+            <Text>공공도서관</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>휴관일 | {rest}</Text>
+            <Text style={{ color: '#3679fe' }}>{price}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => navigation.navigate('Mypage')}
+        >
+          <Text style={{ fontSize: 20, paddingTop: 10, paddingBottom: 10 }}>
+            {name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>{addr}</Text>
+            <Text>공공도서관</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>휴관일 | {rest}</Text>
+            <Text style={{ color: '#3679fe' }}>{price}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => navigation.navigate('Mypage')}
+        >
+          <Text style={{ fontSize: 20, paddingTop: 10, paddingBottom: 10 }}>
+            {name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>{addr}</Text>
+            <Text>공공도서관</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingBottom: 10,
+            }}
+          >
+            <Text>휴관일 | {rest}</Text>
+            <Text style={{ color: '#3679fe' }}>{price}</Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
@@ -161,9 +286,7 @@ const Library = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: 'white',
   },
   header: {
     flexDirection: 'row',
@@ -172,31 +295,31 @@ const styles = StyleSheet.create({
     zIndex: 10,
     justifyContent: 'space-between',
   },
-  navigation: {
+  searchButton: {
     flexDirection: 'row',
     flex: 0.1,
-    alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 6,
     backgroundColor: 'white',
   },
   input: {
     flex: 0.96,
+    flexDirection: 'row',
     marginTop: 10,
     marginBottom: 15,
     borderColor: '#E6F2F4',
-    borderRadius: 14,
+    borderRadius: 10,
     backgroundColor: '#E6F2F4',
     color: '#b6b6b6',
-    padding: 20,
+    justifyContent: 'space-between',
+    width: 327,
+    height: 42,
   },
   touchable: {
     flex: 0.18,
-    flexDirection: 'row',
-    borderTopWidth: 0.25,
     borderColor: '#C2DEEB',
-    alignItems: 'center',
-    paddingLeft: 30,
+    paddingLeft: 40,
+    paddingRight: 40,
+    borderBottomWidth: 0.25,
   },
 });
 export default Library;
